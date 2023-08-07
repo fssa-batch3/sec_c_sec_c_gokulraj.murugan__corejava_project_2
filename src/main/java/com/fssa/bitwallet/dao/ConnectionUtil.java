caa7ded937d1b10a6a6ef3ae7d3d251680f6f414
Gokulraj <110807453+Gokulraj-1@users.noreply.github.com>
2023-08-07 17:08:04 +0530

    Update ConnectionUtil.java

--- src/main/java/com/fssa/bitwallet/dao/ConnectionUtil.java
+++ src/main/java/com/fssa/bitwallet/dao/ConnectionUtil.java
@@ -6,26 +6,35 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class ConnectionUtil {
 	public static Connection getConnection() {
+        Connection con = null;
 
-		Connection con = null;
-		String url = "jdbc:mysql://localhost:3306/bitwallet";
+        String url, userName, passWord;
 
-		String userName = "root";
-		String passWord = "123456";
-		
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriverManager.getConnection(url, userName, passWord);
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new RuntimeException("Unable to connect to the database");
-		}
-		System.out.println("connection successful");
-		return con;
-	}
+        if (System.getenv("CI") != null) {
+            url = System.getenv("DATABASE_HOST");
+            userName = System.getenv("DATABASE_USERNAME");
+            passWord = System.getenv("DATABASE_PASSWORD");
+        } else {
+            Dotenv env = Dotenv.load();
+            url = env.get("DATABASE_HOST");
+            userName = env.get("DATABASE_USERNAME");
+            passWord = env.get("DATABASE_PASSWORD");
+        }
 
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, userName, passWord);
+            System.out.println("Connections created ");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Unable to connect to the database");
+        }
+        return con;
+    }
 	public static void close(Connection conn, Statement stmt, ResultSet rs) {
 
 		try {
